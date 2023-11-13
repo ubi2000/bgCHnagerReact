6310d3a34105ccc76c0f9c03c19fe4f49fc188e0
obaid <obaidlone@gildwaretechnologies.com>
2023-11-13 16:05:14 +0530

    change color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
-const [color,setColor]=useState('green')
+  const [color, setColor] = useState("green");
 
   return (
     <>
-      
-        </>
-  )
+      <div style={{ backgroundColor: color ,width:"70rem",height:"30rem"}}></div>
+      <button onClick={()=>{
+        setColor("red")
+      }}>Click to change color to Red</button>
+      <button onClick={()=>{
+        setColor("white")
+      }}>Click to change color to White</button>
+      <button onClick={()=>{
+        setColor("yellow")
+      }}>Click to change color to Yellow</button>
+      <button onClick={()=>{
+        setColor("olive")
+      }}>Click to change color to olive</button>
+    </>
+  );
 }
 
-export default App
+export default App;
