936ed15eb3626184bd11972b971605394d3b2f07
obaid <obaidlone@gildwaretechnologies.com>
2023-11-13 15:56:44 +0530

    creating project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+const [color,setColor]=useState('green')
+
+  return (
+    <>
+      
+        </>
+  )
+}
+
+export default App
